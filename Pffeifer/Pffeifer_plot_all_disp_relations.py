5307b038c54a7f7522a28fdb5849b78b45c32293
Leila Rocío Prelat <leilaprelat@gmail.com>
2020-02-08 19:52:49 -0300

    Add files via upload

--- Pffeifer/Pffeifer_plot_all_disp_relations.py
+++ Pffeifer/Pffeifer_plot_all_disp_relations.py
@@ -72,10 +72,14 @@ if alfa ==0.1:
 elif alfa ==1:
     list_modos = [1,2,3,4,5,6]
     x = np.linspace(0.45,0.75,300)
+elif alfa ==10:
+    list_modos = np.linspace(1,15,15)
+    x = np.linspace(0.001,0.75,300)
 
 color = 0
 plt.figure(figsize=tamfig)
 for modo_virtual in list_modos:
+    modo_virtual = int(modo_virtual)
     color = color +1
 
     folder = 'alfa' + str(alfa)  
@@ -91,13 +95,21 @@ for modo_virtual in list_modos:
         os.chdir(direc)
         Kz_obt2 = np.loadtxt('mod_virtual' + str(modo_virtual) + '_Kz.txt',delimiter='\t')
         Omega_real_obt2 = np.loadtxt('mod_virtual' + str(modo_virtual) + '_Re_Omega.txt',delimiter='\t')   
-        Omega_imag_obt2 = np.loadtxt('mod_virtual' + str(modo_virtual) + '_Im_Omega.txt',delimiter='\t')  
+        Omega_imag_obt2 = np.loadtxt('mod_virtual' + str(modo_virtual) + '_Im_Omega.txt',delimiter='\t')    
     
-    
-    plt.plot(Kz_obt2,Omega_real_obt2,'.-',ms=8,color=lista_colores[color],alpha=0.7,label='modo = %i' %(modo_virtual)) 
-
+    if alfa!=10:
+        color_graph = lista_colores[color]
+        plt.plot(Kz_obt2,Omega_real_obt2,'.-',color = color_graph, ms=8,alpha=0.7,label='modo = %i' %(modo_virtual)) 
+    else: 
+        plt.plot(Kz_obt2,Omega_real_obt2,'.-', ms=8,alpha=0.7,label='modo = %i' %(modo_virtual)) 
+        
 plt.title('Relación de dispersión. Modos virtuales. alfa = %.1f' %(alfa),fontsize=tamtitle)
-plt.plot(x,x,'.-',ms=8,color=lista_colores[color+1],alpha = 0.7,label='Ω = K')
+if alfa==10:
+    tamlegend = 18
+    plt.plot(x,x,'.-',ms=8,color=lista_colores[0],alpha = 0.7,label='Ω = K')
+    tamlegend = int(tamlegend*0.8)
+else: 
+    plt.plot(x,x,'.-',ms=8,color=lista_colores[color+1],alpha = 0.7,label='Ω = K')
 plt.ylabel('Ω',fontsize=tamletra)
 plt.xlabel('K',fontsize=tamletra)
 plt.tick_params(labelsize = tamnum)
