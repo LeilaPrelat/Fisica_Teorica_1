6a841a9fbcad0e21ade87a08e5b1c6c137f29c81
LeilaPrelat <leilaprelat@gmail.com>
2020-02-13 15:40:59 -0300

    git commit "all"

--- 2medios_plot_all_disp_relation.py
+++ 2medios_plot_all_disp_relation.py
@@ -83,67 +83,67 @@ list_R = np.array(list_R)*1e-9
 
 list_modos = [0,1]
 
+print('Graficar E vs Re(kz)')
 color = 0
 plt.figure(figsize=tamfig)
 for modo in list_modos:
     for R in list_R:
-        if R == 5*1e-9 and modo!=0 or R!= 5*1e-9:
-            color = color +1
+        color = color +1
+    
+        folder_mediums = title2 + '_' + title3
+        folder_R = 'R' + '_' + str(int(R*1e9)) 
+        os.chdir(det_path + '/' + folder_mediums + '/' + folder_R)
         
-            folder_mediums = title2 + '_' + title3
-            folder_R = 'R' + '_' + str(int(R*1e9)) 
-            os.chdir(det_path + '/' + folder_mediums + '/' + folder_R)
+        try:   
+            Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+            kz_real_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Rekz_list.txt',delimiter='\t')   
+        except OSError or IOError as error:
+            print(error)
+            direc = input('path de los .txt de las relaciones de dispersion')
+            os.chdir(direc)
+            Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+            kz_real_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Rekz_list.txt',delimiter='\t')   
             
-            try:   
-                Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
-                kz_real_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Rekz_list.txt',delimiter='\t')   
-            except OSError or IOError as error:
-                print(error)
-                direc = input('path de los .txt de las relaciones de dispersion')
-                os.chdir(direc)
-                Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
-                kz_real_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Rekz_list.txt',delimiter='\t')   
-                
-            kz_real_min_NM2 = np.array(kz_real_min_NM)*1e-8   
-            
-            plt.title(title +', R = %i nm' %(R*1e9),fontsize=tamtitle)
-            plt.plot(np.abs(kz_real_min_NM2),Elist_NM,'.',ms=10,color=lista_colores[color],alpha=0.7,label='modo = %i, R = %i nm' %(modo,int(R*1e9))) 
-            plt.xlabel('Re($k_z$) [x 10$^8$ m$^{-1}$]',fontsize=tamletra)
-            plt.ylabel('Energy [eV]',fontsize=tamletra)
-            plt.tick_params(labelsize = tamnum)
-            plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
-            plt.grid(1)
-            
+        kz_real_min_NM2 = np.array(kz_real_min_NM)*1e-8   
+        
+        plt.title(title +', R = %i nm' %(R*1e9),fontsize=tamtitle)
+        plt.plot(np.abs(kz_real_min_NM2),Elist_NM,'.',ms=10,color=lista_colores[color],alpha=0.7,label='modo = %i, R = %i nm' %(modo,int(R*1e9))) 
+        plt.xlabel('Re($k_z$) [x 10$^8$ m$^{-1}$]',fontsize=tamletra)
+        plt.ylabel('Energy [eV]',fontsize=tamletra)
+        plt.tick_params(labelsize = tamnum)
+        plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
+        plt.grid(1)
+
+print('Graficar E vs Im(kz)')            
 color = 0
 plt.figure(figsize=tamfig)
 for modo in list_modos:
     for R in list_R:
-        if R == 5*1e-9 and modo!=0 or R!= 5*1e-9:
-            color = color +1
+        color = color +1
+    
+        folder_mediums = title2 + '_' + title3
+        folder_R = 'R' + '_' + str(int(R*1e9)) 
+        os.chdir(det_path + '/' + folder_mediums + '/' + folder_R)
         
-            folder_mediums = title2 + '_' + title3
-            folder_R = 'R' + '_' + str(int(R*1e9)) 
-            os.chdir(det_path + '/' + folder_mediums + '/' + folder_R)
+        try:   
+            Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+            kz_imag_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Imkz_list.txt',delimiter='\t')     
+        except OSError or IOError as error:
+            print(error)
+            direc = input('path de los .txt de las relaciones de dispersion')
+            os.chdir(direc)
+            Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+            kz_imag_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Imkz_list.txt',delimiter='\t')  
             
-            try:   
-                Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
-                kz_imag_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Imkz_list.txt',delimiter='\t')     
-            except OSError or IOError as error:
-                print(error)
-                direc = input('path de los .txt de las relaciones de dispersion')
-                os.chdir(direc)
-                Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
-                kz_imag_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Imkz_list.txt',delimiter='\t')  
-                
-            kz_imag_min_NM2 = np.array(kz_imag_min_NM)*1e-8   
-            
-            plt.title(title +', R = %i nm' %(R*1e9),fontsize=tamtitle)
-            plt.plot(np.abs(kz_imag_min_NM2),Elist_NM,'.',ms=10,color=lista_colores[color],alpha=0.7,label='modo = %i, R = %i nm' %(modo,int(R*1e9))) 
-            plt.xlabel('Im($k_z$) [x 10$^8$ m$^{-1}$]',fontsize=tamletra)
-            plt.ylabel('Energy [eV]',fontsize=tamletra)
-            plt.tick_params(labelsize = tamnum)
-            plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
-            plt.grid(1)            
+        kz_imag_min_NM2 = np.array(kz_imag_min_NM)*1e-8   
+        
+        plt.title(title +', R = %i nm' %(R*1e9),fontsize=tamtitle)
+        plt.plot(np.abs(kz_imag_min_NM2),Elist_NM,'.',ms=10,color=lista_colores[color],alpha=0.7,label='modo = %i, R = %i nm' %(modo,int(R*1e9))) 
+        plt.xlabel('Im($k_z$) [x 10$^8$ m$^{-1}$]',fontsize=tamletra)
+        plt.ylabel('Energy [eV]',fontsize=tamletra)
+        plt.tick_params(labelsize = tamnum)
+        plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
+        plt.grid(1)            
 
 
 #%%
--- 3medios_plot_all_disp_relation.py
+++ 3medios_plot_all_disp_relation.py
@@ -83,6 +83,7 @@ title = title1 + '/' + title2 + '/' + title3
 print('Importar la relacion de dispersion')
 
 R2 = 85*1e-9
+folder_mediums = title1 + '_' + title2 + '_' + title3
 
 if ε1==ε_Ag:
     list_R1 = [5,40,70]
@@ -90,6 +91,9 @@ if ε1==ε_Ag:
 elif ε1==ε_CdS:
     list_R1 = [30,70]
     list_modos = [0,1]
+    
+
+print('Graficar E vs Re(kz)')
 
 list_R1 = np.array(list_R1)*1e-9
 color = 0
@@ -98,8 +102,7 @@ for modo in list_modos:
     for R1 in list_R1:
         if R1 == 5*1e-9 and modo!=0 or R1!= 5*1e-9:
             color = color +1
-        
-            folder_mediums = title1 + '_' + title2 + '_' + title3
+                  
             folder_R1 = 'R1' + '_' + str(int(R1*1e9))   
             os.chdir(det_path + '/' + folder_mediums + '/' + folder_R1)
             
@@ -123,6 +126,34 @@ for modo in list_modos:
             plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
             plt.grid(1)
 
+if ε1==ε_Ag:
+    plt.figure(figsize=tamfig)
+    R1 = 5*1e-9 
+    modo = 0
+    folder_R1 = 'R1' + '_' + str(int(R1*1e9))   
+    os.chdir(det_path + '/' + folder_mediums + '/' + folder_R1)
+     
+    try:   
+        Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+        kz_real_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Rekz_list.txt',delimiter='\t')    
+    except OSError or IOError as error:
+        print(error)
+        direc = input('path de los .txt de las relaciones de dispersion')
+        os.chdir(direc)
+        Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+        kz_real_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Rekz_list.txt',delimiter='\t') 
+    
+    kz_real_min_NM2 = np.array(kz_real_min_NM)*1e-8      
+    plt.title(title +', R2 = %i nm' %(R2*1e9),fontsize=tamtitle)
+    plt.plot(np.abs(kz_real_min_NM2),Elist_NM,'.',ms=10,color=lista_colores[color+1],alpha=0.7,label='modo = %i, R1 = %i nm' %(modo,int(R1*1e9))) 
+    plt.xlabel('Re($k_z$) [x 10$^8$ m$^{-1}$]',fontsize=tamletra)
+    plt.ylabel('Energy [eV]',fontsize=tamletra)
+    plt.tick_params(labelsize = tamnum)
+    plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
+    plt.grid(1)
+
+
+print('Graficar E vs Im(kz)')
 color = 0
 plt.figure(figsize=tamfig)
 for modo in list_modos:
@@ -130,7 +161,6 @@ for modo in list_modos:
         if R1 == 5*1e-9 and modo!=0 or R1!= 5*1e-9:
             color = color +1
 
-            folder_mediums = title1 + '_' + title2 + '_' + title3
             folder_R1 = 'R1' + '_' + str(int(R1*1e9))   
             os.chdir(det_path + '/' + folder_mediums + '/' + folder_R1)
             
@@ -153,6 +183,32 @@ for modo in list_modos:
             plt.tick_params(labelsize = tamnum)
             plt.legend(loc='lower right',markerscale=3,fontsize=int(tamlegend*0.9))
             plt.grid(1)
+            
+if ε1==ε_Ag:
+    plt.figure(figsize=tamfig)
+    R1 = 5*1e-9 
+    modo = 0
+    folder_R1 = 'R1' + '_' + str(int(R1*1e9))   
+    os.chdir(det_path + '/' + folder_mediums + '/' + folder_R1) 
+    try:   
+        Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+        kz_imag_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Imkz_list.txt',delimiter='\t')    
+    except OSError or IOError as error:
+        print(error)
+        direc = input('path de los .txt de las relaciones de dispersion')
+        os.chdir(direc)
+        Elist_NM = np.loadtxt('NM_mod' + str(modo) + '_E_list.txt',delimiter='\t')
+        kz_imag_min_NM = np.loadtxt('NM_mod' + str(modo) + '_Imkz_list.txt',delimiter='\t') 
+        
+    kz_imag_min_NM2 = np.array(kz_imag_min_NM)*1e-8
+    plt.title(title +', R2 = %i nm' %(R2*1e9),fontsize=tamtitle)
+    plt.plot(np.abs(kz_imag_min_NM2),Elist_NM,'.',ms=10,color=lista_colores[color+1],alpha=0.7,label='modo = %i, R1 = %i nm' %(modo,int(R1*1e9))) 
+    plt.xlabel('Re($k_z$) [x 10$^8$ m$^{-1}$]',fontsize=tamletra)
+    plt.ylabel('Energy [eV]',fontsize=tamletra)
+    plt.tick_params(labelsize = tamnum)
+    plt.legend(loc='best',markerscale=3,fontsize=tamlegend)
+    plt.grid(1)
+
 
 #%%
     
